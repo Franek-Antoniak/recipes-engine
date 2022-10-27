623de882e8253b187885f6294a7cd24eefef361f
Franek Antoniak <franekant123@gmail.com>
2022-10-27 20:45:45 +0200

    - deleting broken files - some changes with patterns - code cleanup

--- src/main/java/recipes/security/authentication/facade/AuthenticationFacade.java
+++ src/main/java/recipes/security/authentication/facade/AuthenticationFacade.java
@@ -5,5 +5,7 @@ import org.springframework.security.core.Authentication;
 public interface AuthenticationFacade {
 	Authentication getAuthentication();
 
+	String getAuthenticationName();
+
 	void throwIfUserIsNotAuthorised(String username);
 }
