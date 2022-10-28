7d6568e15e93316855f8b7b1a9efa24b89b71403
Franek Antoniak <franekant123@gmail.com>
2022-10-28 19:07:30 +0200

    - postRecipe synchronized with swagger

--- src/main/java/recipes/user/model/UserCreate.java
+++ src/main/java/recipes/user/model/UserCreate.java
@@ -1,5 +1,6 @@
 package recipes.user.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import recipes.user.annotations.ExtendedEmailValidator;
@@ -10,9 +11,13 @@ import javax.validation.constraints.Pattern;
 @Data
 @NoArgsConstructor
 public class UserCreate {
+
+	@ApiModelProperty(notes = "The user's email address", required = true)
 	@NotBlank(message = "Email cannot be blank")
 	@ExtendedEmailValidator
 	private String email;
+
+	@ApiModelProperty(notes = "The user's password", required = true)
 	@NotBlank(message = "Password cannot be blank")
 	@Pattern(regexp = "\\w{8,}", message = "Password should contain at least 8 characters")
 	private String password;
