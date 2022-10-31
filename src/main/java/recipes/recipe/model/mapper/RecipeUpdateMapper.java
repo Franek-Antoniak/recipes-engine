f593b56efa293f5dacc8dff6bc7bbe7821396fc7
Franek Antoniak <franekant123@gmail.com>
2022-10-31 15:02:48 +0100

    - docker - heroku file

--- src/main/java/recipes/recipe/model/mapper/RecipeUpdateMapper.java
+++ src/main/java/recipes/recipe/model/mapper/RecipeUpdateMapper.java
@@ -1,15 +1,16 @@
 package recipes.recipe.model.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 import recipes.recipe.Recipe;
 import recipes.recipe.model.RecipeUpdate;
+
+import java.time.LocalDateTime;
 
 
 @Mapper(componentModel = "spring")
 public abstract class RecipeUpdateMapper {
+	@BeanMapping(nullValuePropertyMappingStrategy =
+			NullValuePropertyMappingStrategy.IGNORE)
 	@Mappings(
 			{@Mapping(
 					target = "id",
@@ -23,8 +24,12 @@ public abstract class RecipeUpdateMapper {
 			), @Mapping(
 					target = "author",
 					ignore = true
-			),}
+			)}
 	)
 	public abstract void updateRecipe(RecipeUpdate recipeUpdate, @MappingTarget Recipe recipe);
+	@AfterMapping
+	protected void setToCurrentDate(@MappingTarget Recipe recipe) {
+		recipe.setDate(LocalDateTime.now());
+	}
 }
 
